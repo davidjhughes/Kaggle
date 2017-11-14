cf547f5bfead9904539261d67963e786c2c622c1
David Hughes <hughesdavidj@gmail.com>
2017-11-13 20:52:04 -0700

    Turned Titanic into a proper project

--- Titanic/TitanicDataAnalysis.R
+++ Titanic/TitanicDataAnalysis.R
@@ -1,5 +1,5 @@
-train <- read.csv("train.csv", header = TRUE)
-test <- read.csv("test.csv", header = TRUE)
+# train <- read.csv("train.csv", header = TRUE)
+# test <- read.csv("test.csv", header = TRUE)
 
 test.survived <- data.frame(Survived = rep("None", nrow(test)), test[,])
 
